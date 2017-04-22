204d7f74502411df75e75ec44f63057f9c35a01e
aliciahu <a7hu@uwaterloo.ca>
2017-04-21 23:48:57 -0400

    Controller: OptionCls class declaration

--- controller/ctrl.h
+++ controller/ctrl.h
@@ -1,7 +1,21 @@
 #ifndef __CTRL_H__
 #define __OPTIONS_CLS__
 class OptionsCls {
-
+	static int option_val;
+public:
+	OptionsCls();
+	void set_no_back_end();
+	void set_list();
+	void set_emit();
+	void set_optimize();
+	void set_peephold();
+	void set_format();
+	static int no_back_end();
+	static int list();
+	static int emit();
+	static int optimize();
+	static int peephole();
+	static int format();
 };
 
 class ControllerCls {
