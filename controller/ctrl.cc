620f9526b7399f432c13a40b5c5b5d96cbecf694
aliciahu <a7hu@uwaterloo.ca>
2017-04-22 00:02:24 -0400

    Controller: OptionCls implementation

--- controller/ctrl.cc
+++ controller/ctrl.cc
@@ -8,8 +8,25 @@ int main (int argc, char **argv) {
 	return 0;	
 }
 
-OptionsCls::OptionsCls() {
-
+int OptionsCls::option_val = 0;
+OptionsCls::OptionsCls() {}
+OptionsCls::set_no_back_end () {
+	option_val |= 0x01;
+}
+OptionsCls::set_list() {
+	option_val |= 0x02;
+}
+OptionsCls::set_emit() {
+	option_val |= 0x04;
+}
+int OptionsCls::no_back_end () {
+	return (option_val & 0x01);
+}
+int OptionsCls::list() {
+	return (option_val & 0x02);
+}
+int OptionsCls::emit() {
+	return (option_val & 0x04);
 }
 
 ControllerCls::ControllerCls() {
