9aab055f199efb452ce9624727f6c2c5cf74ac3f
aliciahu <a7hu@uwaterloo.ca>
2017-04-21 23:42:21 -0400

    Controller: initial commit

---
+++ controller/ctrl.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctrl.h>
+
+using namespace std;
+
+int main (int argc, char **argv) {
+}
+
+OptionsCls::OptionsCls() {
+
+}
+
+ControllerCls::ControllerCls() {
+
+}
---
+++ controller/ctrl.h
@@ -0,0 +1,10 @@
+#ifndef __CTRL_H__
+#define __OPTIONS_CLS__
+class OptionsCls {
+
+};
+
+class ControllerCls {
+
+};
+#endif // __CTRL_H__
